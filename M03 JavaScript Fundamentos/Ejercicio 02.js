6357602089017c25560678124440dae171c5df9e
maricris800 <maricris800@hotmail.com>
2023-09-30 14:27:05 -0400

    Estos son mis avances de la homework del módulo 3 del PrepCourse Henry

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -2,36 +2,46 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
+   return string
 }
+
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+   
+   suma= x + y
+   return(suma)
 }
+
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+   
+   resta= x - y
+   return(resta)
 }
+
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+  
+   divide= x / y
+   return(divide)
 }
+
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+  
+   multiplica= x * y
+   return(multiplica)
 }
 
+
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+   
+   obtenerResto= x % y 
+   return(obtenerResto)
 }
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
