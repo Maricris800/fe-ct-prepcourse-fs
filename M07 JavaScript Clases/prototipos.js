cc641e894eac63e54fd9f8ce2c85506cc4560b14
maricris800 <maricris800@hotmail.com>
2023-10-09 19:55:42 -0400

    estos son los avances del homework modulo 7 del PrepCourse Henry

---
+++ M07 JavaScript Clases/prototipos.js
@@ -0,0 +1,65 @@
+Array.prototype.mayorQueTres = function () {
+    var arregloModificado = [];
+    for (var i = 0; i < this.length; i++) {
+           if (this[ i ] > 3) {
+                  arregloModificado.push(false);
+           } else {
+                  arregloModificado.push(this[ i ]);
+           }
+    }
+    return arregloModificado;
+ };
+  
+ var arreglo = [1, 2, 3, 4, 5];
+ var nuevoArreglo = arreglo.mayorQueTres();
+ console.log(nuevoArreglo);
+
+ class LatinoAmerica {
+	constructor() {
+		this.paises = [ ];
+	};
+};
+ 
+LatinoAmerica.prototype.agregarPais = function (pais) {
+   	this.paises.push(pais);
+};
+let continente = new LatinoAmerica();
+continente.agregarPais('México');
+console.log(continente.paises);
+
+
+//EXTENSION DE CLASES
+class Persona {
+    constructor(nombre, edad) {
+       this.nombre = nombre;
+       this.edad = edad;
+    }
+    saludar() {
+       console.log(
+          'Hola, mi nombre es  ' + this.nombre + '. Tengo  ' + this.edad
+       );
+    }
+ }
+let martin = new Persona('Martin', 26);
+martin.saludar();
+
+class Programador extends Persona {
+    constructor(nombre, edad, añosDeExperiencia) {
+       super(nombre, edad);
+       this.añosDeExperiencia = añosDeExperiencia;
+    }
+    codear() {
+       console.log(
+          'Soy ' +
+             this.nombre +
+             ' . Codeo desde hace  ' +
+             this.añosDeExperiencia +
+             ' años'
+       );
+    }
+ }
+ 
+let martin2 = new Persona('Martín', 26);
+let programador = new Programador('María', 37, 4);
+martin2.saludar();
+programador.codear();
